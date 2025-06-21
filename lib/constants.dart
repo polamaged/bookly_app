ad102d1fe57051abd109fb8f53477abcf9e5c2cd
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-22 00:18:14 +0300

    refactor init state & navigate to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 const Kprimarycolor = Color(0xFF100B20);
+const Ktransationduration = Duration(milliseconds: 250);
--- lib/feature/splash/presentation/views/widget/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widget/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
 import 'package:bookly_app/feature/splash/presentation/views/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -9,36 +13,61 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
-  late AnimationController animationController ;
-  late Animation<Offset> slidingAnimation ;
-@override
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
   void initState() {
-    animationController = AnimationController(vsync: this , duration: const Duration(seconds: 1));
-
-    slidingAnimation = Tween<Offset>(begin: const Offset(0 , 6) , end:Offset.zero ).animate(animationController);
-    animationController.forward();
     super.initState();
+    initslidinganimation();
+
+    navigateToHome();
   }
+
+ 
 
   @override
   void dispose() {
     animationController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment:MainAxisAlignment.center ,
-      crossAxisAlignment:CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-       Image.asset(AssetsData.logo),
-        const SizedBox(height: 4,),
-       SlidingText(slidingAnimation: slidingAnimation),
-
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
+
+  void initslidinganimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 6),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+
+   void navigateToHome() {
+      Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: Ktransationduration,
+      );
+    });
+  }
+  
 }
-
-
