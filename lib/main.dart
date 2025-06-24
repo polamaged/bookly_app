07feabc0e05e68e40b0c96ec729b9762ebe015c5
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-25 00:51:17 +0300

    add font family to our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -15,6 +16,7 @@ class BooklyApp extends StatelessWidget {
     return GetMaterialApp(
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Kprimarycolor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme), 
       ),
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
