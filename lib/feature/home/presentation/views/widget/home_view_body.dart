cea950497ca9ccce6cf88ac3f46b80ce87b5dd6e
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-26 20:15:52 +0300

    best seller item part 3

--- lib/feature/home/presentation/views/widget/home_view_body.dart
+++ lib/feature/home/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/feature/home/presentation/views/widget/best_seller_list_view_item.dart';
 import 'package:bookly_app/feature/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:bookly_app/feature/home/presentation/views/widget/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -27,44 +28,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.TestImage),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width *.4,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire ',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
