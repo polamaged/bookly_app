7fafe81d63bee798b6f2a66bbb0a04681534aa23
polamaged <74374673+polamaged@users.noreply.github.com>
2025-07-05 21:48:44 +0300

    create newest book cubit state

---
+++ lib/feature/home/presentation/manger/newest_books/newest_books_state.dart
@@ -0,0 +1,25 @@
+part of 'newest_books_cubit.dart';
+
+abstract class NewestBooksState extends Equatable {
+  const NewestBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NewestBooksInitial extends NewestBooksState {}
+class NewestBooksLoading extends NewestBooksState {}
+class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+
+  const NewestBooksSuccess({required this.books});
+
+
+}
+class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  const NewestBooksFailure({required this.errMessage});
+
+
+}
